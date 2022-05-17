86c79f1fb9c58a49d90d873a4370f2c9f6ef67b6
tallywiesenberg <t.j.wies@gmail.com>
2022-05-17 09:53:19 -0400

    filecoin insurance first pass

--- contracts/FilecoinInsurance.sol
+++ contracts/FilecoinInsurance.sol
@@ -9,12 +9,7 @@ contract FilecoinInsurance is UsingTellor, ERC20 {
 
     string queryType;
 
-    struct Claim {
-        mapping(address => uint256) payments;
-        bool live;
-    }
-
-    mapping(bytes32 => Claim) public claims;
+    mapping(bytes32 => mapping(address => uint256)) public claims;
 
     constructor(address payable _tellorAddress) UsingTellor(_tellorAddress) ERC20("FilecoinInsurance", "FCI") {
         queryType = "FilecoinDealStatus";
@@ -27,16 +22,36 @@ contract FilecoinInsurance is UsingTellor, ERC20 {
 
         require(success, "request to Tellor unsuccessful");
 
+        bool status = abi.decode(rawStatus, (bool));
+
+        uint256 claimAmount = claims[keccak256(abi.encode(_proposalCID))][msg.sender];
+
         if (status == false) {
-            claims[keccak256(_proposalCID)].live = false;
-            claims[keccak256(_proposalCID)][msg.sender] = 0;
+            claims[keccak256(abi.encode(_proposalCID))][msg.sender] = 0;
         }
 
-        transferFrom(address(this), msg.sender);
+        transferFrom(address(this), msg.sender, claimAmount);
 
     }
 
-    function requestCoverage(string memory _proposalCID) external {
+    function requestCoverage(string memory _proposalCID, uint256 _amount) external {
+
+        bytes memory queryData = abi.encode(queryType, abi.encode(_proposalCID));
+        bytes32 queryId = keccak256(queryData);
+        (bool success, bytes memory rawStatus, uint256 timestamp) = getDataBefore(queryId, block.timestamp - 3 hours);
+
+        require(success, "request to Tellor unsuccessful");
+
+        bool status = abi.decode(rawStatus, (bool));
+        require(status, "Filecoin deal is non-active!");
+        //set claim 
+        claims[keccak256(abi.encode(_proposalCID))][msg.sender] = _amount;
+
+        //approve
+        approve(msg.sender, _amount);
+
+        //transfer from
+        transferFrom(msg.sender, address(this), _amount);
 
 
     }
